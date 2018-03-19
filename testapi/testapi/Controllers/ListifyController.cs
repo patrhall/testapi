e17e54004fed902b15f01bed6a4ca36b83045ab3
Patrik Hallner <patrik.hallner@ascom.com>
2018-03-19 14:59:13 +0100

    2

---
+++ testapi/testapi/Controllers/ListifyController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace testapi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ListifyController : Controller
+    {
+        // GET api/values
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET api/listify/5/10/7
+        [HttpGet("{start}/{end}/{index}")]
+        public object Get(int start, int end, int index)
+        {
+            var noInList = start + index;
+            if (noInList >= start - 1 && noInList <= end - 1)
+            {
+                var list = new Listify(start, end);
+                return list[index];
+            }
+            else
+            {
+                return "Wrong";
+            }  
+        }
+
+        //// POST api/values
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ testapi/testapi/Listify.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testapi
+{
+    class Listify : IList
+    {
+        private object[] _contents = new object[0];
+        private int _count;
+
+        public Listify(int start, int end)
+        {
+            _contents = new object[end - start];
+            for (var i = start; i <= end; i++)
+            {
+                Add(i);
+            }
+        }
+
+        public int Add(object value)
+        {
+            if (_count < _contents.Length)
+            {
+                _contents[_count] = value;
+                _count++;
+
+                return (_count - 1);
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public bool Contains(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                return _contents[index];
+            }
+            set
+            {
+                _contents[index] = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public bool IsFixedSize => throw new NotImplementedException();
+
+        public object SyncRoot => throw new NotImplementedException();
+
+        public bool IsSynchronized => throw new NotImplementedException();
+    }
+}
